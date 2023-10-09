b208ae1669df624f028c8c42afa106693f25f804
SonyaRodina <sonya-rodina1@mail.ru>
2023-10-09 13:29:11 +0400

    'scrollIntoView'

--- shop/shop.js
+++ shop/shop.js
@@ -2,12 +2,20 @@ let inputText = document.querySelector('.inputText');
 let submitForm = document.querySelector('.submitForm');
 let information = document.querySelectorAll('.search-element');
 
+function go_scroll(block) {
+    block.scrollIntoView({
+        block: 'center',
+        behavior: 'smooth'
+    })
+}
+
 const searchFunction=(text)=> {
         information.forEach((element)=> {
             if(element.textContent == text) {
                 element.style = 'animation:animationText 2s;';
                 element.parentElement.style = 'animation:animationCard 2s;'
                 console.log(element);
+                go_scroll(element);
             }
         });
     }
